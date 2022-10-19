41a5cded7e29934c7fd54f611028c5794a16d54c
Jordan Eggers <66223278+Jordanseggers@users.noreply.github.com>
2022-10-19 17:15:47 -0400

    finish job views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima a alias id vero doloribus. Iusto atque veritatis, dolore consequuntur alias quisquam dignissimos doloremque cupiditate tenetur porro qui dolorum adipisci id?</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
